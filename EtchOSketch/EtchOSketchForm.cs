3aa0ed6d68d17cffd53ad8ee3b236a0d0c889cf1
abeldoyl <abeldoyle71@gmail.com>
2026-04-26 16:59:30 -0600

    Add project files.

---
+++ EtchOSketch/EtchOSketchForm.cs
@@ -0,0 +1,10 @@
+namespace EtchOSketch
+{
+    public partial class EtchOSketchForm : Form
+    {
+        public EtchOSketchForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
